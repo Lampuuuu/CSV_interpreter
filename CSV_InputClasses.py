314240a52bfc997c890e4b10dbefda6f091a3ca6
Lampuuuu <philipplampert@hotmail.de>
2023-01-21 00:41:34 +0100

    einige Testdateien, sowie erste Laufende Version

--- CSV_InputClasses.py
+++ CSV_InputClasses.py
@@ -1,33 +1,49 @@
 import os
-import tkinter
+import tkinter as tk
+
 
 class InputClass:
-    __filestring = ""
+    
+    def __init__(self):
+        self.input_filename()
 
     def get_filestring(self):
-        return self.__filestring  
+        return self.__filename  
 
-    def check_file(self):
-        if not os.path.isfile(self.__filestring):
+    def set_filestring(self, filename):
+        self.__filename = filename
+
+    def check_file(self, filename):
+        if not os.path.isfile(filename):
             raise Exception("Datei konnte nicht gefunden werden")
+        elif filename[-3:].lower() != "csv":
+            raise Exception("Es handelt sich bei der Datei um keine CSV")
+        else:
+            print("Dateieingabe korrekt!")
 
     def get_input_file(self):
         try: 
             self.check_file()
-            return self.__filestring
+            return self.__filename
         except Exception as e:
             print(f"{e}")
-
+    
+    def input_filename(self):
+        self.set_filestring("THIS IS THE WRONG CLASS")
     
 class TextInput(InputClass):
 
     def __init__(self):
-        self.__filestring = input("Bitte geben Sie den genauen Dateistring der CSV-Datei an:\n")
+        self.__filename = input("Bitte geben Sie den genauen Dateistring der CSV-Datei an:\n")
 
 
 class GUI_Input(InputClass):
-    def __init__(self):
-        pass
+    InputWindow = tk.TK()
+    InputWindow.title("Dateieingabe")
+    InputWindow.resizable(False, False)
+    InputWindow.geometry("300x150")
+
+
 
 def main():
     csv_input = TextInput()
